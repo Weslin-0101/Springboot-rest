a4e2486ee590e484fa72273d3e0db507344da559
Wesley Lira <wesleeylirac12@gmail.com>
2023-05-27 09:19:37 -0300

    chore: add FlyWay dependency

---
+++ springboot-rest/src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,14 @@
+-- public.person definition
+
+-- Drop table
+
+-- DROP TABLE public.person;
+
+CREATE TABLE public.person (
+                               id bigserial NOT NULL,
+                               first_name varchar(80) NOT NULL,
+                               last_name varchar(80) NOT NULL,
+                               address varchar(100) NOT NULL,
+                               gender varchar(6) NOT NULL,
+                               CONSTRAINT person_pkey PRIMARY KEY (id)
+);
---
+++ springboot-rest/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.person
+(address, first_name, gender, last_name)
+VALUES ('Brasilia-DF', 'Wesley', 'Gender', 'Lira');
