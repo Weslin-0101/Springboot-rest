c0cd7ea957a26ce2c5c2b07068fd419e6aecdf04
Wesley Lira <wesleeylirac12@gmail.com>
2023-05-29 20:20:40 -0300

    feat: created Table Permission, Users ans User_Permission

---
+++ springboot-rest/src/main/resources/db/migration/V5__Create_Table_Users.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE IF NOT EXISTS public.users (
+    id bigserial NOT NULL,
+    user_name varchar(255) UNIQUE,
+    full_name varchar(255),
+    password varchar(255),
+    account_non_expired boolean,
+    account_non_locked boolean,
+    credentials_non_expired boolean,
+    enabled boolean
+);
+
+ALTER TABLE public.users ADD CONSTRAINT uk_users_id UNIQUE (id);
---
+++ springboot-rest/src/main/resources/db/migration/V7__Create_Table_User_Permission.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE IF NOT EXISTS public.user_permission (
+    id_user bigserial NOT NULL,
+    id_permission bigserial NOT NULL,
+    CONSTRAINT pk_user_permission PRIMARY KEY (id_user),
+    CONSTRAINT uk_user_permission UNIQUE (id_permission),
+    CONSTRAINT fk_user_permission FOREIGN KEY (id_user) REFERENCES public.users (id),
+    CONSTRAINT fk_user_permission_permission FOREIGN KEY (id_permission) REFERENCES public.permission (id)
+);
