96100fbca01cdaeb9281e7c6639e9a41fad12bbe
Wesley Lira <wesleeylirac12@gmail.com>
2023-05-29 23:25:25 -0300

    feat: added AuthController

---
+++ springboot-rest/src/main/java/br/com/rest/services/AuthServices.java
@@ -0,0 +1,48 @@
+package br.com.rest.services;
+
+import br.com.rest.data.vo.v1.security.AccountCredentialsVO;
+import br.com.rest.data.vo.v1.security.TokenVO;
+import br.com.rest.repositories.UserRepository;
+import br.com.rest.securityJwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServices {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @SuppressWarnings("rawtypes")
+    public ResponseEntity signin(AccountCredentialsVO data) {
+        try {
+            var username = data.getUsername();
+            var password = data.getPassword();
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+            var user = userRepository.findByUsername(username);
+            var tokenResponse = new TokenVO();
+
+            if (user != null) {
+                tokenResponse = jwtTokenProvider.createAccessToken(username, user.getRoles());
+            } else {
+                throw new UsernameNotFoundException("Username " + username + " not found");
+            }
+
+            return ResponseEntity.ok(tokenResponse);
+        } catch (Exception e) {
+            throw new BadCredentialsException("Invalid username/password supplied");
+        }
+    }
+}
