f2a18e73ff17b1aab6848e6800c54ff50702c153
Wesley Lira <wesleeylirac12@gmail.com>
2023-06-04 10:25:38 -0300

    feat: created download file endpoint

--- springboot-rest/src/main/java/br/com/rest/services/FileStorageServices.java
+++ springboot-rest/src/main/java/br/com/rest/services/FileStorageServices.java
@@ -2,7 +2,10 @@ package br.com.rest.services;
 
 import br.com.rest.config.FileStorageConfig;
 import expections.FileStorageException;
+import expections.MyFileNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -41,6 +44,20 @@ public class FileStorageServices {
             return filename;
         } catch (Exception e) {
             throw new FileStorageException("Could not store file" + filename + ". Please try again!", e);
+        }
+    }
+
+    public Resource loadFileAsResource(String filename) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(filename).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if(resource.exists()) {
+                return resource;
+            } else {
+                throw new MyFileNotFoundException("File not found " + filename);
+            }
+        } catch (Exception e) {
+            throw new MyFileNotFoundException("File not found " + filename, e);
         }
     }
 }
