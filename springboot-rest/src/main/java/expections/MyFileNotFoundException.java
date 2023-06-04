486f436b5de516c1be6c139d31c35a4c0551dd5e
Wesley Lira <wesleeylirac12@gmail.com>
2023-06-04 09:40:19 -0300

    feat: created exceptions about fileResponse

---
+++ springboot-rest/src/main/java/expections/MyFileNotFoundException.java
@@ -0,0 +1,21 @@
+package expections;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class MyFileNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public MyFileNotFoundException(String msg) {
+        super(msg);
+    }
+
+    public MyFileNotFoundException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+}
